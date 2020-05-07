9412a938a1f918edce93a4c78ae9a5f98bb9d600
naoya7076 <naoya7076@gmail.com>
2020-05-07 16:45:55 +0900

    implement component using multi states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-const App = () => {
+import React, { useState } from 'react';
+const App = props => {
+
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
   return (
-    <div>
-      This is a template for React hooks.
-    </div>
+    <>
+    <p>現在の{name}は{price}円です</p>
+    <button onClick={() => setPrice(price + 1)}>+1</button>
+    <button onClick={() => setPrice(price - 1)}>-1</button>
+    <button onClick={reset}>Reset</button>
+    <input value={name} onChange={e => setName(e.target.value)}/>
+    </>
       );
 }
+
+App.defaultProps = {
+  name: 'test',
+  price: 1000
+};
 
 export default App;
