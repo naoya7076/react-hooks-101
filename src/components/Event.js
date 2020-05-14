4f18dda094b905fe60f10c43a08c4c5160d352c3
naoya7076 <naoya7076@gmail.com>
2020-05-14 21:47:17 +0900

    solve props drilling issue

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { DELETE_EVENT } from '../actions';
+import AppContext from '../contexts/AppContext';
 
-const Event = ({event, dispatch}) => {
-    const id = event.id;
-      const handleClickDeleteButton = () => {
-          const result = window.confirm(`イベント(ID=${id})を本当に削除しますか？`);
-          if (result) dispatch({type:DELETE_EVENT, id});
-      };
-      return (
-        <tr>
-          <td>{id}</td>
-          <td>{event.title}</td>
-          <td>{event.body}</td>
-          <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
-        </tr>
-      )
+const Event = ({ event }) => {
+  const { dispatch } = useContext(AppContext);
+  const id = event.id;
+    const handleClickDeleteButton = () => {
+        const result = window.confirm(`イベント(ID=${id})を本当に削除しますか？`);
+        if (result) dispatch({type:DELETE_EVENT, id});
+    };
+    return (
+      <tr>
+        <td>{id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+        <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
+      </tr>
+    )
 };
 
 export default Event;
